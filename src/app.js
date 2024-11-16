e8450df4f45eff85c8105e246fc3fb899ec0bc50
RishabhRawat2003 <rawatrishabh953@gmail.com>
2024-11-16 19:38:26 +0530

    adds allowed origins

--- src/app.js
+++ src/app.js
@@ -3,10 +3,22 @@ import cors from 'cors'
 
 const app = express()
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+const allowedOrigins = ["https://jaipur-jwellers-frontend.vercel.app", "http://localhost:5173",]; // Add any other origins you need
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            // Allow requests with no origin (like mobile apps or curl requests)
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                // If the origin isn't in the allowed list
+                return callback(new Error("Not allowed by CORS"), false);
+            }
+            return callback(null, true);
+        },
+        credentials: true, // if your frontend needs to send cookies
+    })
+);
 app.use(express.json({ limit: '16kb' }))
 app.use(express.urlencoded({ extended: true, limit: '16kb' }))
 app.use(express.static('public'))
