437bbb02f5e70912fc169728d6f9038ebfbbfa5c
RishabhRawat2003 <rawatrishabh953@gmail.com>
2024-12-14 13:05:47 +0530

    blogs backend done

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,6 +1,7 @@
 import { Admin } from "../model/admin.model.js";
 import jwt from 'jsonwebtoken'
-
+import { Blog } from "../model/blogs.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 //this logic is to add admin credentials to backend
 export const addAdmin = async (req, res) => {
@@ -22,8 +23,6 @@ export const addAdmin = async (req, res) => {
     return res.status(200).json({ message: "Admin added successfully" })
 
 }
-
-
 
 // this logic is to login admin
 export const login = async (req, res) => {
@@ -52,4 +51,93 @@ export const login = async (req, res) => {
     return res
         .status(200)
         .json({ message: "Authentication Complete", token, id: user._id });
+}
+
+export const addBlog = async (req, res) => {
+    const { blogTitle, blogContent } = req.body
+
+    if (!blogTitle || !blogContent || !req.files.image) {
+        return res.status(401).json({ error: "Title, Category, tag and Image is required" })
+    }
+    const image = await uploadOnCloudinary(req.files.image[0].path)
+
+    const blog = await Blog.create({
+        title: blogTitle,
+        content: blogContent,
+        image: image.secure_url,
+    })
+
+    if (!blog) {
+        return res.status(401).json({ error: "Blog not founded" })
+    }
+
+    return res
+        .status(200)
+        .json({ message: "blog created successfully" });
+}
+
+// this is to get all blogs
+export const getBlogs = async (req, res) => {
+    const blog = await Blog.find()
+
+    if (!blog) {
+        return res.status(401).json({ error: "Blogs not founded" })
+    }
+
+    return res
+        .status(200)
+        .json({ message: blog });
+}
+
+// to delete all blogs
+export const deleteAllBlogs = async (req, res) => {
+    const blog = await Blog.deleteMany()
+
+    if (!blog) {
+        return res.status(401).json({ error: "Blogs not founded" })
+    }
+
+    return res
+        .status(200)
+        .json({ message: "all deleted" });
+}
+
+// this is to get single blog
+export const getSingleBlog = async (req, res) => {
+    const { id } = req.body
+
+    if (!id) {
+        return res.status(401).json({ error: "Id not founded" })
+    }
+
+    const blog = await Blog.findById(
+        id
+    )
+
+    if (!blog) {
+        return res.status(401).json({ error: "Blogs not founded" })
+    }
+
+    return res
+        .status(200)
+        .json({ message: blog });
+}
+
+// this is to delete single blog
+export const deleteSingleBlog = async (req, res) => {
+    const { id } = req.body
+
+    if (!id) {
+        return res.status(401).json({ error: "Id not founded" })
+    }
+
+    const blog = await Blog.findByIdAndDelete(id)
+
+    if (!blog) {
+        return res.status(401).json({ error: "Blogs not founded" })
+    }
+
+    return res
+        .status(200)
+        .json({ message: "Blog deleted Successfully" });
 }
